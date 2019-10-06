3da36b6280d2646658d100a723f5fd62032d5036
stevenzearo <2226069315@qq.com>
2019-10-06 18:53:37 +0800

    init

--- hdfs/src/main/scala/steve/hdfs/app/Main.scala
+++ hdfs/src/main/scala/steve/hdfs/app/Main.scala
@@ -5,7 +5,8 @@ import steve.hdfs.app.hdfs.HDFSClient
 object Main {
   def main(args: Array[String]) :Unit = {
     val client = new HDFSClient("hdfs://localhost:8020", "steve")
-    client.uploadFile("/home/steve/IdeaProjects/bigdata/hdfs/src/main/resources/test.txt", "/test/upload/", deleteSrc = false, overwrite = true)
+//    client.uploadFile("/home/steve/IdeaProjects/bigdata/hdfs/src/main/resources/test.txt", "/test/upload/", deleteSrc = false, overwrite = true)
+    client.downloadFile("/test/upload", "/home/steve/IdeaProjects/bigdata/hdfs/src/main/resources/download")
     client.close()
   }
 }
