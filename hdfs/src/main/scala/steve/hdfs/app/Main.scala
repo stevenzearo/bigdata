586baeec5c7e9dd8a1f47e3a8d73b203900748a4
stevenzearo <2226069315@qq.com>
2019-10-07 19:26:09 +0800

    hdfs process

--- hdfs/src/main/scala/steve/hdfs/app/Main.scala
+++ hdfs/src/main/scala/steve/hdfs/app/Main.scala
@@ -7,7 +7,7 @@ object Main {
     val client = new HDFSClient("hdfs://localhost:8020", "steve")
 //    client.uploadFile("/home/steve/IdeaProjects/bigdata/hdfs/src/main/resources/test.txt", "/test/upload/", deleteSrc = false, overwrite = true)
     client.downloadFile("/test/upload", "/home/steve/IdeaProjects/bigdata/hdfs/src/main/resources/download")
-    
+
     client.close()
   }
 }
